dbbecf0afd1e4c29348379a525f62e8019c67556
Andrea Crevacore <andreacrevacore@gmail.com>
2025-10-21 16:22:38 +0200

    si

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolioUS/', // -> sostituisci con il nome del repo oppure './' per path relativi
+  base: './',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
